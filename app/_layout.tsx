9a1a067787783e97adf4e4a814aebd69ef1524e0
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2025-01-02 16:32:40 +0200

    Implement authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,107 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+    DarkTheme,
+    DefaultTheme,
+    ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { useSegments, useRouter, Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { Session } from '@supabase/supabase-js';
+
+import { supabase } from '@/utils/supabase';
+
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+    // Ensure that reloading on `/modal` keeps a back button present.
+    initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        ...FontAwesome.font,
+    });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+    return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" />
-      </Stack>
-    </ThemeProvider>
-  );
+    const [session, setSession] = useState<Session | null>(null);
+    const [initialized, setInitialized] = useState<boolean>(false);
+
+    const segments = useSegments();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!initialized) return;
+
+        // Check if the path/url is in the (auth) group
+        const inAuthGroup = segments[0] === '(tabs)';
+
+        if (session && !inAuthGroup) {
+            // Redirect authenticated users to the list page
+            //@ts-ignore
+            router.replace('/(tabs)/');
+        } else if (!session) {
+            // Redirect unauthenticated users to the login page
+            router.replace('/');
+        }
+    }, [session, initialized]);
+
+    useEffect(() => {
+        // Listen for changes to authentication state
+        const { data } = supabase.auth.onAuthStateChange(
+            async (event, session) => {
+                console.log(
+                    'supabase.auth.onAuthStateChange',
+                    event,
+                    session,
+                );
+
+                setSession(session);
+                setInitialized(true);
+            },
+        );
+        return () => {
+            data.subscription.unsubscribe();
+        };
+    }, []);
+
+    return (
+        <ThemeProvider
+            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+        >
+            <Slot />
+        </ThemeProvider>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,129 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+    Button,
+    Alert,
+} from 'react-native';
+import React, { useState } from 'react';
 
-const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+import { supabase } from '@/utils/supabase';
+
+function Auth() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    // Sign in with email and password
+    const onSignInPress = async () => {
+        setLoading(true);
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) Alert.alert(error.message);
+        setLoading(false);
+    };
+
+    // Create a new user
+    const onSignUpPress = async () => {
+        setLoading(true);
+        const {
+            data: { session },
+            error,
+        } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+
+        if (error) Alert.alert(error.message);
+        if (!session)
+            Alert.alert(
+                'Please check your inbox for email verification!',
+            );
+
+        setLoading(false);
+    };
+
+    const signInAnonymously = async () => {
+        const { data, error } =
+            await supabase.auth.signInAnonymously();
+        if (error) Alert.alert(error.message);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Nutrisi</Text>
+            <TextInput
+                autoCapitalize="none"
+                placeholder="john@doe.com"
+                value={email}
+                onChangeText={setEmail}
+                style={styles.inputField}
+            />
+            <TextInput
+                placeholder="password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                style={styles.inputField}
+            />
+            <TouchableOpacity
+                style={styles.button}
+                onPress={onSignInPress}
+            >
+                <Text style={styles.buttonText}>Sign in</Text>
+            </TouchableOpacity>
+            <Button
+                onPress={onSignUpPress}
+                title="Create Account"
+                color={'#64867e'}
+            />
+            <Button
+                onPress={signInAnonymously}
+                title="Skip >>"
+                color={'#64867e'}
+            />
+        </View>
+    );
 }
 
-export default index
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#d2e4e1',
+        alignItems: 'center',
+        paddingTop: 100,
+    },
+    header: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#000',
+        paddingVertical: 48,
+    },
+    inputField: {
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 5,
+        width: '80%',
+        margin: 10,
+    },
+    button: {
+        marginVertical: 15,
+        alignItems: 'center',
+        backgroundColor: '#64867e',
+        padding: 12,
+        borderRadius: 4,
+        width: '80%',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 17,
+    },
+});
+
+export default Auth;
